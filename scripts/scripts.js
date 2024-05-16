55ddc69b26d4c19cf95881255b218ab859250745
Lucas Mendonça <lms.devop@gmail.com>
2024-05-16 11:43:32 -0300

    Finish details

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,4 +4,10 @@ const navElement = document.querySelector('#animateme');
 hamburguerElement.addEventListener('click', () => {
     navElement.classList.toggle('open');
     hamburguerElement.classList.toggle('open');
-})
+});
+
+const myBtn = document.querySelector('#darkBtn');
+const mainElemnent = document.querySelector('#main');
+myBtn.addEventListener('click', () => {
+    mainElemnent.classList.toggle('dark');
+});
